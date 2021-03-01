a49f6b61ad62b6f2ab41323c7168dcfb1b46825e
jihyeonmun <jihyeonmun@gmail.com>
2021-03-01 21:33:36 +0900

    Fourth Draft(search, pageable) modified,

--- src/main/java/com/example/demo/repository/BoardRepository.java
+++ src/main/java/com/example/demo/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +11,6 @@ public interface BoardRepository extends JpaRepository<Board,Long> {
     List<Board> findByTitle(String title);
     List<Board> findByTitleOrContent(String title, String content);
 //    docs.spring.io/spring-data/jap/docs/2.3.1.RELEASE/reference/html/#reference
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
+
 }
