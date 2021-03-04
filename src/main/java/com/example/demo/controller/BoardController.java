404b25d66b65f9fc5c12c71cab64b2ca39b62695
jihyeonmun <jhmoon1994@gmail.com>
2021-03-04 15:20:11 +0900

    Error Modified

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -32,7 +32,7 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping("/list")
-    public String list(Model model, @PageableDefault(size =5) Pageable pageable,
+    public String list(Model model, @PageableDefault(size =10) Pageable pageable,
                        @RequestParam(required = false, defaultValue = "") String searchText) {
 //        Page<Board> boards = boardRepository.findAll(pageable);
         Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
